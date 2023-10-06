99368e14fd2cfb23ad8e88f0e7cf85fa8ee57dbc
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-06 16:32:26 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers followed by ,
+ *
+ * Return: Always 0(Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+
+	return (0);
+}
