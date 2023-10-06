dfa4fa29fe1cdaddedd328268d5f09d6e1c1650b
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-05 19:59:18 -0700

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: print a quote using write function
- * 	write(int fd, const void *buf,size_t count)
+ * Description: print a quote using wrote function
+ *	ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: 1 (not success)
 */
-
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, quo, 59);
 	return (1);
 }
