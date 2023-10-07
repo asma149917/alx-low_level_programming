579206a2c10c65b8d950db507214a9bab05cb6b5
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-06 21:21:28 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program using puts fuction
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/*Your Code*/
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
