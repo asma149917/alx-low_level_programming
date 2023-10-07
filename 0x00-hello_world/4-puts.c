2bb88b0e7196e2bacb13e9e534e4cbd4c1c4ffd4
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-06 21:23:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,6 @@
 int main(void)
 {
 	/*Your Code*/
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
