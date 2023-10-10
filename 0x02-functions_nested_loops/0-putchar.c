629efc14114bd7842322450e7138e77c351b3d59
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-10 15:15:28 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Using _putchar to print
+ *
+ * Return: Always 0(Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+		_putchar(str[c]);
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 
 int _putchar(char c);
 void print_alphabet(void);
+
 #endif
