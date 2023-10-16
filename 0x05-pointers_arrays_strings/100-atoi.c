da639c6814bf78552f823967908818a6928dd789
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-15 23:51:52 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,17 +10,26 @@
 
 int _atoi(char *s)
 {
-	unsigned int num = 0;
-	int sign = 1;
+	int c = 0;
+	unsigned int ni = 0;
+	int isi = 0;
+	int min = 1;
 
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= 9)
-			num = (num * 10) + (*s - '0');
-		else if (num > 0)
+	while (s[c])
+	{
+		if (s[c] == 45)
+			min *= -1;
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - 0);
+			c++;
+		}
+		if (isi == 1)
 			break;
-	} while (*s++);
+		c++;
+	}
+	ni *= min;
 
-	return (num * sign);
+	return (ni);
 }
