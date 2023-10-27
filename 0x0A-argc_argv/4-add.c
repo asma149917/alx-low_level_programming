ff256871da61badec1a9572093035c0adcbfb013
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-27 02:27:26 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,13 +15,12 @@
 int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
-	char *c;
 
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j])
+			if (!isdigit(argv[i][j]))
 					{
 						printf("Error\n");
 						return (1);
