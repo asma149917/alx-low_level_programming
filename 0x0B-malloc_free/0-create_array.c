a7202bbf62afffd66e68199de71ad876354f291c
asma149917 <asmaa.wajeah1410@gmail.com>
2023-10-30 13:27:53 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,17 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(size);
+	unsigned int i;
+	char *s;
 
-	if (size == 0 || n == NULL)
-		return (0);
-	while (size--)
-		n[size] = c;
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
 }
