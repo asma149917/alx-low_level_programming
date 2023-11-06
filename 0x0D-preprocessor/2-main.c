031f947c7fe7ea38adc6889ed1c89fd91e38c022
asma149917 <asmaa.wajeah1410@gmail.com>
2023-11-06 04:59:31 -0800

    A

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the name of the file.
  *
- * Description: prints the name of the file it was
- *              compiled from, followed by a new line
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
